75563bd6cf6b66a0dd339fddbd8773da2f802db4
Radu <radu.caragin-crasovan@student.upt.ro>
2023-07-20 17:13:28 +0300

    Tema3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,22 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import java.util.Properties;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] csv) {
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Read Excel")
+                .master("local")
+                .getOrCreate();
 
         SparkSession sparkSession = SparkSession.builder().master("local").appName("Read_Erasmus").getOrCreate();
         Dataset<Row> dataset=  sparkSession.read().option("header","true").csv("E:/practica_IBM/JavaProj/src/main/resources/Erasmus.csv");
@@ -18,6 +29,18 @@ public class Main {
         dataset.printSchema();
         dataset.select("Receiving Country Code","Sending Country Code").show(20,false);
         dataset.groupBy("Receiving Country Code","Sending Country Code").count().withColumnRenamed("count","Number of students").show();
+
+        Properties prop=new Properties();
+        prop.setProperty("Username","root");
+        prop.setProperty("Password","root");
+
+        dataset.groupBy("Receiving Country Code","Sending Country Code")
+                .count()
+                .orderBy("Receiving Country Code","Sending Country Code")
+                .write()
+                .jdbc("jdbc:mysql://localhost:3306/erasmus","table",prop);
+
+
 
         }
     }
